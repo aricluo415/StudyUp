b4dcae89791acc42613eaa421cea1c84dd04794d
Kevin Leong <roflitzkevin@hotmail.com>
2019-02-11 01:23:54 -0800

    Added test cases for EventServiceImplTest.Java

--- src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
+++ src/test/java/edu/studyup/serviceImpl/EventServiceImplTest.java
@@ -76,4 +76,142 @@ class EventServiceImplTest {
 		  });
 	}
 	
+	@Test
+	void testUpdateEventNameLength_badCase() throws StudyUpException {
+		int eventID = 1; 
+		String eventName = "Event name is longer than 20 for testing";
+		//assertTrue(eventName.length() <= 20 , "Event name is bigger than 20 for Testing");
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.updateEventName(eventID, eventName);
+		});
+	}
+	
+	//Good Test
+	@Test
+	void testGetActiveEvents_GoodCase(){ 
+		Event event = new Event();
+		event.setEventID(2);
+		event.setDate(new Date(100000000));
+		event.setName("Event 2");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		DataStorage.eventData.put(event.getEventID(), event);
+		assertTrue(eventServiceImpl.getActiveEvents().contains(DataStorage.eventData.get(2)));
+	}
+	
+	//Bad Case: The Date is 1997 which still is still returned in the Active Event List
+	@Test
+	void testGetActiveEvents_BadCase(){ 
+		Event event = new Event();
+		event.setEventID(2);
+		event.setDate(new Date(97,1,1));
+		event.setName("Event 2");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		DataStorage.eventData.put(event.getEventID(), event);
+		assertFalse(eventServiceImpl.getActiveEvents().contains(DataStorage.eventData.get(2)));
+	}
+	
+	//Good Case
+	@Test
+	void testAddStudentToEvent_GoodCase() throws StudyUpException { 
+		int eventID = 1;
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(eventID);
+		//Event event1 = eventServiceImpl.addStudentToEvent(student, eventID);
+		//List<Student> listOfStudents = DataStorage.eventData.get(eventID).getStudents();
+		assertTrue(eventServiceImpl.addStudentToEvent(student, eventID).getStudents().contains(student));
+	}
+	
+	//Good Case to test Null StudentsList
+	@Test
+	void testAddStudentToEvent_GoodCase_StudentsNull() throws StudyUpException { 
+		int eventID = 2;
+		Event event = new Event();
+		event.setEventID(eventID);
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(eventID);
+		DataStorage.eventData.put(eventID, event);
+		assertTrue(eventServiceImpl.addStudentToEvent(student, eventID).getStudents().contains(student));
+	}
+	
+	//Bad Case: The Event does not exist
+	@Test
+	void testAddStudentToEvent_BadCase_EventNull() { 
+		//Event event = new Event();
+		//event.setEventID(2);
+		//DataStorage.eventData.put(event.getEventID(), null);
+	
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(2);
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student, 2);
+		  });
+	}
+	//Bad Case: Adds more than two students per Event
+	@Test
+	void testAddStudentToEvent_BadCase_MoreThan2() throws StudyUpException{
+		int eventID = 1;
+		Student student1 = new Student();
+		Student student2 = new Student();
+		student1.setId(eventID);
+		student2.setId(eventID);
+		eventServiceImpl.addStudentToEvent(student1, eventID);
+		assertFalse(eventServiceImpl.addStudentToEvent(student2, eventID).getStudents().contains(student2));
+	}
+	
+	//Bad Case: Cannot add the same student to same Event
+	@Test
+	void testAddStudentToEvent_BadCase_SameStudent() throws StudyUpException { 
+		int eventID = 1;
+		Student student = new Student();
+		student.setFirstName("John");
+		student.setLastName("Doe");
+		student.setEmail("JohnDoe@email.com");
+		student.setId(eventID);
+	
+		assertFalse(eventServiceImpl.addStudentToEvent(student, eventID).getStudents().contains(student));
+
+	}
+	
+	//Bad Case: Event ID does not exist
+	@Test
+	void testAddStudentToEvent_BadCase_WrongEventID() {
+		Student student = new Student();
+		student.setFirstName("Jane");
+		student.setLastName("Doe");
+		student.setEmail("JaneDoe@email.com");
+		student.setId(2);
+		Assertions.assertThrows(StudyUpException.class, () -> {
+			eventServiceImpl.addStudentToEvent(student, 2);
+		  });
+	}
+	
+	@Test 
+	void testDeleteEvent_GoodCase () {
+		eventServiceImpl.deleteEvent(1);
+		assertTrue(DataStorage.eventData.isEmpty());
+	}
+	
+	@Test
+	void testGetPastEvents_GoodCase() {
+		Event event = new Event();
+		event.setEventID(2);
+		event.setDate(new Date(97,1,1));
+		event.setName("Event 2");
+		Location location = new Location(-122, 37);
+		event.setLocation(location);
+		DataStorage.eventData.put(event.getEventID(), event);
+	
+		assertTrue(eventServiceImpl.getPastEvents().contains(DataStorage.eventData.get(2)));
+	}
 }
